e2ae32f23d6d9097f6d4c0c19e0d2829b8d708d4
shibano <shibano@tmi.t.u-tokyo.ac.jp>
2023-02-17 14:01:40 +0900

    途中

--- merkle/src/merkle_tree.rs
+++ merkle/src/merkle_tree.rs
@@ -1,38 +1,50 @@
 use crate::prelude::*;
-use crate::Hasher;
+use halo2curves::pasta::Fp;
 use num_bigint::BigUint;
 use std::collections::HashMap;
+use std::vec::Vec;
 
-pub struct SparseMerkleTree<T: Hasher> {
-    leaves: HashMap<BigUint, T::Hash>,
+pub struct SparseMerkleTree {
+    leaves: HashMap<BigUint, Fp>,
+    layers: Vec<Vec<One>>,
 }
-
-impl<T: Hasher> Default for SparseMerkleTree<T> {
+pub struct Leaf {
+    address: BigUint,
+    hash: Fp,
+}
+pub struct One {
+    is_left: bool,
+    hash: Fp,
+}
+impl Default for SparseMerkleTree {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl<T: Hasher> SparseMerkleTree<T> {
+impl SparseMerkleTree {
     pub fn new() -> Self {
         Self {
             leaves: HashMap::new(),
+            layers: Vec::new(),
         }
     }
 
-    // pub fn root(&self) -> Option<T::Hash> {
-    // }
+    pub fn include(&self, address: BigUint, leaf: Fp) -> Vec<One> {
+        let ret = Vec::new();
 
-    pub fn include(&self, address: BigUint, leaf: T::Hash) -> bool {
+        return ret;
+    }
+
+    pub fn exclude(&self, address: BigUint, leaf: Fp) -> bool {
         true
     }
 
-    pub fn exclude(&self, address: BigUint, leaf: T::Hash) -> bool {
-        true
-    }
+    pub fn update(&mut self, leaf: Leaf) -> &mut Self {
+        self.leaves.insert(leaf.address, leaf.hash);
 
-    pub fn update(&mut self, address: BigUint, leaf: T::Hash) -> &mut Self {
-        self.leaves.insert(address, leaf);
+        // ここでlayerを構築
+
         self
     }
 }
