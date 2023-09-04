ebdf26045e5670604f9baaa207ec7f2e18a89748
shibano <shibano@tmi.t.u-tokyo.ac.jp>
2023-09-04 11:21:58 +0900

    localdb

---
+++ localdb/create_tables.sql
@@ -0,0 +1,17 @@
+drop table if exists account;
+create table account(
+    _id integer not null primary key,
+    public_key_for_eddsa text not null,
+    created_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
+    updated_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
+);
+create unique index idx_account_01 on account(public_key_for_eddsa);
+
+drop table if exists state;
+create table state(
+    _id integer not null primary key,
+    balance_encrypted text not null,
+    created_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
+    updated_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
+);
+
