726086358a94b425c05b1dbf1c9b78360dd7934c
ashWhiteHat <phantomofrotten@gmail.com>
2023-09-11 10:56:35 +0900

    sucess compile and test

--- verifier-contract/contracts/Rollup.sol
+++ verifier-contract/contracts/Rollup.sol
@@ -1,15 +1,77 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Rollup {
-    address yulVerifier;
+import { IRollup } from "./IRollup.sol";
 
-    constructor(address _yulVerifier) {
-        yulVerifier = _yulVerifier;
+contract Rollup is IRollup {
+    // on-chain users state root
+    bytes merkleRoot;
+    // deposit transaction index
+    uint64 depositIndex;
+    // individual number index
+    uint64 individualNumberIndex;
+    // ayer 2 operator address
+    address operator;
+    // deposit function verifier contract
+    address depositVerifier;
+    // batch function verifier contract
+    address batchVerifier;
+    // withdraw function verifier contract
+    address withdrawVerifier;
+
+    // on-chain withdraw Merkle tree key is EdDSA address
+    mapping(address => withdrawInfo) withdrawTreeInfo;
+
+    // on-chain deposit Merkle tree key is `depositIndex`
+    mapping(uint64 => leafInfo) depositTreeInfo;
+
+    /**
+     * on-chain deposit information
+     * individualNumber individual number index
+     * jubjubAddress EdDSA depositor address
+     */
+    struct leafInfo {
+        uint64 individualNumber;
+        address jubjubAddress;
     }
 
+    constructor(address _batchVerifier) {
+        operator = msg.sender;
+        batchVerifier = _batchVerifier;
+    }
+
+    function deposit(
+        address from,
+        uint256 public_key_x,
+        uint256 public_key_y,
+        uint32 amount,
+        uint256 left_cipher_x,
+        uint256 left_cipher_y,
+        uint256 right_cipher_x,
+        uint256 right_cipher_y,
+        bytes calldata proof
+    ) external {}
+
+    function withdraw(
+        address to,
+        address from,
+        uint32 amount,
+        uint256 left_cipher_x,
+        uint256 left_cipher_y,
+        uint256 right_cipher_x,
+        uint256 right_cipher_y,
+        bytes calldata proof
+    ) external {}
+
+    function batch(
+        bytes memory current_root,
+        bytes memory new_root,
+        bytes calldata transactions,
+        bytes calldata proof
+    ) external {}
+
     function verify(bytes calldata input) external view returns (bool) {
-        (bool success, ) = yulVerifier.staticcall(input);
+        (bool success, ) = batchVerifier.staticcall(input);
         return success;
     }
 }
